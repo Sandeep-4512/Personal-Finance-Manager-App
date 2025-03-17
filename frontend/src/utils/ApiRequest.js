73922459d5f9916f7d23a3d785a0e2556beae89a
Sandeep Reddy 4512 <sandeep4512reddy@gmail.com>
2025-03-17 21:43:48 +0530

    Updated API requests with deployed backend URL

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -7,3 +7,4 @@ export const addTransaction = `${host}/api/v1/addTransaction`;
 export const getTransactions = `${host}/api/v1/getTransaction`;
 export const editTransactions = `${host}/api/v1/updateTransaction`;
 export const deleteTransactions = `${host}/api/v1/deleteTransaction`;
+console.log("Backend API URL:", process.env.REACT_APP_API_URL);
